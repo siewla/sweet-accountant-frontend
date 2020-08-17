94b6f9ccd1229e282e224b35ab2cdf7e5a580587
siewla <siewlapang@gmail.com>
2020-08-17 22:57:23 +0800

    add categories icon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
 import Main from './pages/notLogin/Main'
+import Categories from './components/Categories'
 
 function App() {
   return (
     <Fragment>
       <Main />
+      <Categories />
     </Fragment>
   );
 }
---
+++ src/components/Categories.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Categories = () => {
+    return (
+        <div>
+            <h3><span class="material-icons">fastfood</span>Food & Drink</h3>
+            <h3><span class="material-icons">home</span>House</h3>
+            <h3><span class="material-icons">directions_bus</span>Transportation</h3>
+            <h3><span class="material-icons">flight_takeoff</span>Travel</h3>
+            <h3><span class="material-icons">shopping_cart</span>Groceries</h3>
+            <h3><span class="material-icons">live_tv</span>Entertainment</h3>
+            <h3><span class="material-icons">sports_handball</span>Sports</h3>
+            <h3><span class="material-icons">school</span>Education</h3>
+            <h3><span class="material-icons">medical_services</span>Medical</h3>
+            <h3><span class="material-icons">school</span>Education</h3>
+            <h3><span class="material-icons">shopping_bag</span>Shopping</h3>
+            <h3><span class="material-icons">offline_bolt</span>Utilities</h3>
+            <h3><span class="material-icons">loyalty</span>Donations</h3>
+        </div>
+    )
+}
+
+export default Categories
