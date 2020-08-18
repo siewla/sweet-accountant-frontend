1e3f68934d1593e8144c8bb9f370cc9fa6c7853f
siewla <siewlapang@gmail.com>
2020-08-18 23:22:34 +0800

    tested pie chart from react-google-charts

--- src/pages/isLogin/Main.jsx
+++ src/pages/isLogin/Main.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import Pie from '../../components/charts/Pie'
 
 const Main = () => {
+    const data=[
+                ['Categories', 'Amount($)'],
+                ['Food & Drink', 200.19],
+                ['House', 500],
+                ['Car', 400.99],
+                ['Travel', 201.11],
+            ]
+            
     return (
         <div>
             <div>
@@ -13,6 +22,7 @@ const Main = () => {
                 <button className="btn btn-outline-white wow fadeInLeft" data-wow-delay="0.3s">Reports</button>
                 <button className="btn btn-outline-white wow fadeInLeft" data-wow-delay="0.3s">Import</button>
             </div>
+            <Pie data={data} title='First Pie Chart' />
         </div>
     )
 }
