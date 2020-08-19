0b9634ec7db6798eb11da8501e844de5c7695639
linhle <linhle@Hieus-MacBook-Air.local>
2020-08-19 16:35:02 +0800

    add basic authentication, create dashboard page

--- src/components/landing/Header.jsx
+++ src/components/landing/Header.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import Introduction from './Introduction';
 import Nav from './Nav';
-import Signup from './Signup';
-import Login from './Login';
-import Dashboard from '../pages/isLogin/Main'
-import Chatbot from './chatbot/CustomChatBox';
+import Signup from '../authentication/Signup';
+import Login from '../authentication/Login';
+// import Dashboard from '../../pages/isLogin/Main'
+import Chatbot from '../chatbot/CustomChatBox';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-const Header = () => {
+const Header = (props) => {
+    const login = props.login;
     return (
         <div>
             <Router>
-                <header>
+                <header className="header">
                     <Nav />
                     <Switch>
                     <Route path="/" exact component={Introduction} />
                         <Route path="/signup" component={Signup} />
-                        <Route path="/login" component={Login} />
-                        <Route path='/dashboard' component={Dashboard} /> 
+                        <Route path="/login" render={() => <Login login={login}/> }/>
+                        {/* <Route path='/dashboard' component={Dashboard} />  */}
                         {/* just test the buttons on the left */}
                     </Switch>
                     <Chatbot />
