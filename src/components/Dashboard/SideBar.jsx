6acc8cbe355aa4fca64f508a6c841005390334a1
linhle <linhle@Hieus-MacBook-Air.local>
2020-08-20 22:02:59 +0800

    add accounts page

--- src/components/Dashboard/SideBar.jsx
+++ src/components/Dashboard/SideBar.jsx
@@ -1,36 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const SideBar = () => {
+const SideBar = (props) => {
+    const changeCurrentContent = props.changeCurrentContent;
     return (
         <div className="sidebar-fixed position-fixed">
             <a className="logo-wrapper waves-effect" href="/">
                 <img src="../../../img/sweet.png" className="img-fluid" alt="" />
             </a>
             <div className="list-group list-group-flush">
-                
+
                 <p className="list-group-item active waves-effect">
                     <i className="fas fa-chart-pie mr-3"></i>Dashboard</p>
 
-                    <Link to="/login">
-                <p className="list-group-item list-group-item-action waves-effect">
-                    <i className="fas fa-user mr-3"></i>Profile</p>
-                    </Link>
+                <Link to="/login" >
+                    <p className="list-group-item list-group-item-action waves-effect" onClick={() => {changeCurrentContent('Profile')}}>
+                        <i className="fas fa-user mr-3"></i>Profile</p>
+                </Link>
 
-                <p className="list-group-item list-group-item-action waves-effect">
+                <p className="list-group-item list-group-item-action waves-effect" onClick={() => {changeCurrentContent('All Transactions')}}>
                     <i className="fas fa-money-bill-alt mr-3"></i>Transactions</p>
 
-                <p className="list-group-item list-group-item-action waves-effect">
-                <i className="fas fa-table mr-3"></i>Categories</p>
+                <p className="list-group-item list-group-item-action waves-effect" onClick={() => {changeCurrentContent('Categories')}}>
+                    <i className="fas fa-table mr-3"></i>Categories</p>
 
-                <p className="list-group-item list-group-item-action waves-effect">
-                <i className="fas fa-wallet mr-3"></i>Accounts</p>
+                <Link to="/accounts">
+                    <p className="list-group-item list-group-item-action waves-effect" onClick={() => {changeCurrentContent('All Accounts')}}>
+                        <i className="fas fa-wallet mr-3"></i>Accounts</p>
+                </Link>
+                <p className="list-group-item list-group-item-action waves-effect" onClick={() => {changeCurrentContent('Reports')}}>
+                    <i className="far fa-chart-bar mr-3"></i>Reports</p>
 
-                <p className="list-group-item list-group-item-action waves-effect">
-                <i className="far fa-chart-bar mr-3"></i>Reports</p>
-
-                <p className="list-group-item list-group-item-action waves-effect">
-                <i className="fas fa-file-import mr-3"></i>Imports</p>
+                <p className="list-group-item list-group-item-action waves-effect" onClick={() => {changeCurrentContent('Import')}}>
+                    <i className="fas fa-file-import mr-3"></i>Import</p>
             </div>
 
         </div>
