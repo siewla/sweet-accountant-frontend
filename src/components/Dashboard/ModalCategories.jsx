f78e6374721008ba1b4629b8e947341709ed7405
linhle <linhle@Hieus-MacBook-Air.local>
2020-08-21 11:53:31 +0800

    add categories

---
+++ src/components/Dashboard/ModalCategories.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import TabCategories from './TabCategories';
+
+const ModalCategories = () => {
+    return (
+        <div class="modal fade" id="modalContactForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header text-center">
+                    <h4 class="modal-title w-100 font-weight-bold">Categories</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body mx-3">
+                    <TabCategories />
+            </div>
+        </div>
+    </div>
+    </div>
+    )
+}
+
+export default ModalCategories;
