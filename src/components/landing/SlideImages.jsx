d0553dfd7408cc01dca791a0028f6792ac7b7910
linhle <linhle@Hieus-MacBook-Air.local>
2020-09-04 16:56:42 +0800

    add log in with fb

--- src/components/landing/SlideImages.jsx
+++ src/components/landing/SlideImages.jsx
@@ -5,11 +5,11 @@ const SlideImages = () => {
         <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img class="d-block w-100" src="https://thinkingfocus.com/wp-content/uploads/2018/03/Calculator-880x518.jpg"
+                <img class="d-block w-100" src="https://mediashower.com/blog/wp-content/uploads/2017/03/calculate-1.jpg"
                     alt="First slide" />
             </div>
             <div class="carousel-item">
-                <img class="d-block w-100" src="https://mediashower.com/blog/wp-content/uploads/2017/03/calculate-1.jpg"
+                <img class="d-block w-100" src="https://thinkingfocus.com/wp-content/uploads/2018/03/Calculator-880x518.jpg"
                     alt="Second slide" />
             </div>
             <div class="carousel-item">
